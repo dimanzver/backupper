aaffeffd73754b83823275fbf40834c67c77244f
Diman <diman.suvor.linda.2000@gmail.com>
2022-05-10 20:59:50 +0300

    pdo options - throw errors

--- db.php
+++ db.php
@@ -9,7 +9,12 @@ class DB
     public static function getConnection()
     {
         if (!self::$connection) {
-            self::$connection = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASSWORD);;
+            self::$connection = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASSWORD, [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_ORACLE_NULLS => PDO::NULL_NATURAL,
+                PDO::ATTR_STRINGIFY_FETCHES => false,
+                PDO::ATTR_EMULATE_PREPARES => false,
+            ]);
         }
         return self::$connection;
     }
